0ef553c7d8bcc47a919c671ee355dd03498dba7e
Stefano <stefanoputelli>
2024-08-12 17:20:24 +0200

    [UPDATE]

--- proc.py
+++ proc.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 OUTPUT_PATH = 'out/'
 DATA_PATH = 'data/'
 
 LOW_TH = 2.0
 HIGH_TH = 2.8
-
-R = 380000
 
 def convert_to_scientific_notation(value_str):
     # Definire i fattori di conversione per i vari prefissi
@@ -50,14 +50,19 @@ def calculate_V(Tl, Th, delta_t, R, C):
 
 def load_data(file_path):
     # Load data from a CSV file using pandas
-    data = pd.read_csv(file_path)
-    return data
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        data = []
+        for line in lines:
+            line = line.strip().split(',')
+            data.append([float(line[0]), int(line[1]), int(line[2])])
+        return data
 
-#remove all lines with last column as 0
-def remove_zero(data):
-    # Remove all rows where the last column value is 0
-    data = data[data.iloc[:,-1] != 0]
-    return data
+# #remove all lines with last column as 0
+# def remove_zero(data):
+#     # Remove all rows where the last column value is 0
+#     data = data[data.iloc[:,-1] != 0]
+#     return data
 
 #write data to csv file
 def write_data(data, file_path, output_path):
@@ -84,42 +89,79 @@ def divide_data(data):
 
 def calculate_time(data):
     # Calculate SMP timings and main time from the data
-    smp_start_time = 0
+    smp_start_time = -9999
     smp_timings = []
-    main_time = abs(data.iloc[-1, 0] - data.iloc[0, 0])
+    # main_time = abs(data.iloc[-1, 0] - data.iloc[0, 0])
     for i in range(0, len(data)):
-        if(smp_start_time == 0):
-            if(data.iloc[i, 1] == 1):
-                smp_start_time = data.iloc[i, 0]
+        if(smp_start_time == -9999):
+            if(data[i][2] == 1):
+                smp_start_time = data[i][0]
         else:
-            if(data.iloc[i, 1] == 0):
-                smp_timings.append(abs(smp_start_time - data.iloc[i, 0]))
-                smp_start_time = 0
+            if(data[i][2] == 0):
+                smp_timings.append(abs(smp_start_time - data[i][0]))
+                smp_start_time = -9999
 
-    return smp_timings, main_time
+    return smp_timings #main_time
 
 def start_analysis(file):
     # Perform analysis on a given file
     data = load_data(DATA_PATH + file)
-    data = remove_zero(data)
-    divdata = divide_data(data)
-    for i in range(0, len(divdata)):
-        smp_timings, main_time = calculate_time(divdata[i])
-        smp_average = sum(smp_timings) / len(smp_timings)
-    write_data(divdata, file, OUTPUT_PATH + file[:-4] + "/")
-    return smp_average, main_time
+    # data = remove_zero(data)
+    # divdata = divide_data(data)
+    timings = calculate_time(data)
+    return timings # for i in range(0, len(data)):
+    #     smp_timings, main_time = calculate_time(data)
+    #     print("SMP timings: " + str(smp_timings))
+    #     print("Main time: " + str(main_time))
+    #     smp_average = sum(smp_timings) / len(smp_timings)
+    # #write_data(divdata, file, OUTPUT_PATH + file[:-4] + "/")
+    # return smp_average, main_time
+
 
 if __name__ == '__main__':
     #iterate in all files inside data folder
-    for file in os.listdir(DATA_PATH):
-        smp_average, main_time = start_analysis(file)
+    
+    accuracies = []
+    resistances = []
+    capacitances = []
+    actual_powers = []  # New list to store actual powers
+
+    for file in tqdm(os.listdir(DATA_PATH)):
+        smp_timings = start_analysis(file)
+        smp_average = sum(smp_timings) / len(smp_timings)
         print("SMP average time: " + str(smp_average))
-        print("Main time: " + str(main_time))
         #get C from filename example 1mF-2.2uF.csv -> 2.2uF -> 2.2e-6
-        V = calculate_V(LOW_TH, HIGH_TH, smp_average, R, convert_to_scientific_notation(file.split("-")[1][:-5]))
-        print("P: " + str(pow(V, 2)/R))
-        print("P: " + str(3.3 / R))
+        R = convert_to_scientific_notation(file.split("-")[2][:-6])
+        C = convert_to_scientific_notation(file.split("-")[1][:-1])
+        V = calculate_V(LOW_TH, HIGH_TH, smp_average, R, C)
+        print("V: " + str(V))
+        power_predicted = pow(V, 2) / R
+        actual_power = pow(3.3, 2) / R  # Calculate actual power
+        print("P predicted: " + str(power_predicted))
+        print("P actual: " + str(actual_power))
+        accuracy = (actual_power - abs(power_predicted - actual_power)) / actual_power * 100
+        accuracies.append(power_predicted)  # Append power_predicted instead of accuracy
+        resistances.append(R)
+        capacitances.append(C)
+        actual_powers.append(actual_power)  # Append actual_power to the list
+        print("Accuracy: " + str(accuracy) + "%")
         print(file + " done")
+
+        # #DEBUG
+        # break
+
+    # Plotting the efficiency vs resistance
+    plt.figure()
+    plt.scatter(resistances, accuracies, c=capacitances)
+    plt.scatter(resistances, actual_powers, color='red', label='Actual Power')  # Add a line for actual powers
+    plt.xlabel('Resistance')
+    plt.ylabel('Power Predicted')
+    plt.title('Power Predicted vs Resistance')
+    plt.colorbar(label='Capacitance')
+    plt.legend()  # Add a legend for the line
+    plt.ylim(bottom=0)  # Set the y-axis minimum value to 0
+    plt.show()
+
 
     print("All files done")
 
